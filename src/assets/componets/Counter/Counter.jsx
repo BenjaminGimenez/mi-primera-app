97685f207335478f377d4ac48d95dd9cb37aa8e0
Benjamin Gimenez <benjagimenez824@gmail.com>
2024-06-18 20:37:05 -0300

    agrego ejemplo de counter

--- src/assets/componets/Counter/Counter.jsx
+++ src/assets/componets/Counter/Counter.jsx
@@ -3,16 +3,33 @@ import { useState } from "react";
 const Counter = () => {
     const [count, setCount] = useState(0)
    
-    const handleClick = () =>{
-        console.log('agrego uno')
-        setCount(count + 1)
-        console.log(`ahora es ${count}`)
+  //   const handleSum = () =>{
+  //       console.log('agrego uno')
+  //       setCount(count + 1)
+  //       console.log(`ahora es ${count}`)
+  //   }
+
+  //   const handleLess = () =>{
+  //     console.log('resto uno')
+  //     setCount(count - 1)
+  //     console.log(`ahora es ${count}`)
+  // }
+
+  const handleClick =(operator)=>{
+    let newValue = 0;
+    if(operator === 'sum')
+      newValue = count +1;
+    else{
+      newValue = count -1;
     }
+    setCount(newValue);
+  }
+
   return (
     <section>
         <p>el contador va: {count}</p>
-        <button onClick={handleClick}> + </button>
-        <button>-</button>
+        <button onClick={()=>handleClick('sum')}> + </button>
+        <button onClick={()=>handleClick('less')}>-</button>
     </section>
   )
 }
