3f407b53985e91ab348a3184f30773f3a09247e1
Benjamin Gimenez <benjagimenez824@gmail.com>
2024-06-20 13:13:26 -0300

    usando hooks useState y useRef para lista de tareas

--- src/assets/componets/TodoList/TodoList.jsx
+++ src/assets/componets/TodoList/TodoList.jsx
@@ -1,11 +1,33 @@
-// import { useState } from "react"
+import { useRef } from "react"
+import { useState } from "react"
 
 const TodoList = () => {
 
-    // const [todos, setTodos] = useState([])
+    const [todos, setTodos] = useState([])
 
+    const inputRef = useRef();
+
+    const handleNewTodo = () =>{
+      console.log('cargando..')
+      console.log(inputRef.current.value)
+    }
   return (
-    <div>TodoList</div>
+    <section>
+      {/* Creacion de tareas */}
+      <fieldset>
+        <label>nueva tarea</label>
+        <input type="text" ref={inputRef}/>
+        <button onClick={handleNewTodo}>cargar</button>
+      </fieldset>
+      {/* Listado de tareas */}
+      <ul>
+        {
+          todos.map((todo, index)=>{
+            return <li key={index}>{todo}</li>
+          })
+        }
+      </ul>
+    </section>
   )
 }
 export default TodoList
