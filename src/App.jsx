f820c855a0a70469b314b59b1712e17369a4d16f
Benjamin Gimenez <benjagimenez824@gmail.com>
2024-06-24 14:08:40 -0300

    agrego useEffect en ejemplo de counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css'
-import TodoList from './assets/componets/TodoList/TodoList';
+// import TodoList from './assets/componets/TodoList/TodoList';
 
-// import Counter from './assets/componets/Counter/Counter';
+import Counter from './assets/componets/Counter/Counter';
 // import CardsGallery from './assets/componets/CardsGallery/CardsGallery';
 
 const App = () => {
   return <>
   <h1>hola mundo</h1>
   {/* <CardsGallery/> */}
-  {/* <Counter/> */}
-  <TodoList/>
+  <Counter/>
+  {/* <TodoList/> */}
   
   </>
 };
--- src/assets/componets/Counter/Counter.jsx
+++ src/assets/componets/Counter/Counter.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Counter = () => {
     const [count, setCount] = useState(0)
+    const [countSecundario] = useState(0)
    
   //   const handleSum = () =>{
   //       console.log('agrego uno')
@@ -24,6 +25,32 @@ const Counter = () => {
     }
     setCount(newValue);
   }
+
+
+  //caso prohibido -> no pasarle un arreglo de dependencias
+  
+
+  //caso 1 
+  //se ejecuta 1 vez el callback cuando el componente de monta
+  useEffect(()=>{
+    console.log('me estoy ejecutando cuando se monta el componente')
+  },[]);
+
+  //caso 2 -> depende de una variable o una funcion
+  //se ejecuta el callback cuantas veces la variable o funcion cambie
+  useEffect(()=>{
+    console.log(count)
+  },[count]);
+
+  //caso 2 -> depende de una o mas variables o funciones
+  //se ejecuta el callback cuantas veces las variables o funciones cambien
+  //con que 1 se actualice ya es suficiente para correr el callback
+  useEffect(()=>{
+    console.log('count es ', count)
+    console.log('count secundario es ', countSecundario)
+  },[count, countSecundario]);
+
+
 
   return (
     <section>
