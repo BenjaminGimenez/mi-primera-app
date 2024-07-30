e287986b4c4825be3fce5ac2a3367cd4d5d98169
Benjamin Gimenez <benjagimenez824@gmail.com>
2024-07-30 19:50:50 -0300

    agrego header con navegacion interna

---
+++ src/assets/componets/Common/Header/Header.jsx
@@ -0,0 +1,31 @@
+import { Link, NavLink } from "react-router-dom"
+
+const Header = () => {
+
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container">
+    <Link className="navbar-brand" to='/'>Agenda</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <NavLink className={({isActive})=>{
+            return isActive ? 'nav-link active' : 'nav-link';
+          }} to="/">Inicio</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className={({isActive})=>{
+            return isActive ? 'nav-link active' : 'nav-link';
+          }} to="/contactos">Contactos</NavLink>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  )
+}
+
+export default Header
--- src/assets/views/RootView.jsx
+++ src/assets/views/RootView.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Header from "../componets/Common/Header/Header";
 
 const RootView = () => {
   return (
     <>
-    <header>este es el header</header>
-    <main className="container">
+    <Header/>
+    <main className="container my-3">
       <Outlet/>
     </main>
     <footer>este es el footer</footer>
